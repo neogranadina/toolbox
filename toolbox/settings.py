3b10a3c7875b002df118971e597b741eaed56ef0
root <root@abc.neogranadina.org>
2024-04-17 20:06:14 +0000

    settings updated

--- toolbox/settings.py
+++ toolbox/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-hj(gd649lak)wlen@f%ve&m39^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.22.139.213', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['172.22.139.213', '127.0.0.1', 'localhost', 'toolbox.abcng.org']
 
 
 # Application definition
